7940a8207bf80887252100e930fe7516759b55b1
Vlado <vlado_web@abv.bg>
2019-07-27 18:41:43 +0300

    Change env

--- config/config.js
+++ config/config.js
@@ -3,7 +3,10 @@ const env=require('dotenv').config();
 module.exports = {
     development: {
         port: process.env.PORT|| 3000,
-        dbPath: process.env.DB_CONNECTION,
+        dbPath: process.env.DB_CONNECTION_MLAB,
     },
-    production: {}
+    production: {
+        port: process.env.PORT|| 3000,
+        dbPath: process.env.DB_CONNECTION_MLAB,
+    }
 };
