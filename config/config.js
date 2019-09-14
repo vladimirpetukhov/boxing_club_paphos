40e9a241c792fb11b9a105130ba831707e9bffa0
Vladimir Petukhov <v.petukhov@gisexplorer.eu>
2019-09-14 09:17:51 +0300

    Config change

--- config/config.js
+++ config/config.js
@@ -2,11 +2,11 @@ const env=require('dotenv').config();
 
 module.exports = {
     development: {
-        port: process.env.PORT|| 3000,
+        port: process.env.PORT,
         dbPath:'mongodb://vladimirpetukhov:AaBbCc123!@ds245647.mlab.com:45647/heroku_09khfp12',
     },
     production: {
-        port: process.env.PORT|| 3000,
+        port: process.env.PORT,
         dbPath: 'mongodb://vladimirpetukhov:AaBbCc123!@ds245647.mlab.com:45647/heroku_09khfp12',
     }
 };
